ad50d5c12c8e1e8c4845354f32fcdf806823e977
Jesse Theisen <theiseja@gmail.com>
2019-12-09 09:14:08 -0600

    Updates notes.py with new notes

--- notes.py
+++ notes.py
@@ -1,3 +1,22 @@
+# Lists
+# group of items we ref by index
+# are mutable (mutable can be changed, immutable cannot be changed)
+# can have duplicate items
+
+# creates a new list
+new_list = ["red", "orange", "yellow"]
+
+# add vs insert
+new_list.append("blue") # adds new item to end
+new_list.insert("green") # insert at i
+
+# remove vs pop vs del
+new_list.remove("orange") # simply removes an item
+new_list.pop() # removes the last item in the list
+del new_list[1] # removes item at i
+
+
+
 # Tuples
 
 # creates tuple
