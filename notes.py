5430c3afe6bec1e54be15d71e1e1920ad410f481
Jesse Theisen <theiseja@gmail.com>
2019-12-09 09:06:04 -0600

    Updates notes.py

---
+++ notes.py
@@ -0,0 +1,12 @@
+# Tuples
+
+# creates tuple
+new_tuple = ('item', 'item', 'item')
+
+# prints length of tuple
+print( len(new_tuple))
+
+# traverses through tuple
+for item in new_tuple:
+    # access an item
+    print(item)
